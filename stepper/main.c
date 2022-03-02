2491c597b312313be0da448f566d7c494324640c
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-03-02 16:47:41 -0500

    Ejemplo motor paso a paso unipolar

--- stepper/main.c
+++ stepper/main.c
@@ -2,10 +2,10 @@
 
 int main()
  { 
-  PORTB |= 1<<0;
+  PORTB |= 1<<0; //habilitar pullUp de pin 0 puerto B
   pasoapasoinit(1,2,3,4,100); //pines a usar para movimiento de motor paso a paso y velocidad
   while (1){
-     if ((PINB&(1<<0))==0){
+     if (!(PINB&(1<<0))){  //Se pregunta si el pin 0D se encuentre en bajo
 	giro_izq();
      }
      else{
