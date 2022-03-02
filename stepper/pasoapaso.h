0cd2ec157b8fdce7d72ccda37d1a1f92aee0ed7e
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-03-02 16:07:43 -0500

    libreria paso a paso unipolar

---
+++ stepper/pasoapaso.h
@@ -0,0 +1,8 @@
+
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+
+void pasoapasoinit(uint8_t p01, uint8_t p02, uint8_t p03, uint8_t p04, uint8_t tiem);
+void giro_izq();
+void giro_der();
