4e0653cc528f7bf6e7687082778079c2ee8d83e9
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-03-02 16:45:58 -0500

    Update main.c

--- stepper/main.c
+++ stepper/main.c
@@ -3,7 +3,7 @@
 int main()
  { 
   PORTB |= 1<<0;
-  pasoapasoinit(1,2,3,4,100);
+  pasoapasoinit(1,2,3,4,100); //pines a usar para movimiento de motor paso a paso y velocidad
   while (1){
      if ((PINB&(1<<0))==0){
 	giro_izq();
