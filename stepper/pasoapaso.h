42d27c196dede4ab196b4aacbb2069c23cba3023
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-04-06 18:04:17 -0500

    Update pasoapaso.h

--- stepper/pasoapaso.h
+++ stepper/pasoapaso.h
@@ -3,6 +3,9 @@
 #include <avr/sleep.h>
 #include <util/delay.h>
 
-void pasoapasoinit(uint8_t p01, uint8_t p02, uint8_t p03, uint8_t p04, uint8_t tiem);
+uint8_t p1, p2, p3, p4, demora, Port;
+char pport;
+
+void pasoapasoinit(char port, uint8_t p01, uint8_t p02, uint8_t p03, uint8_t p04, uint8_t tiem);
 void giro_izq();
 void giro_der();
