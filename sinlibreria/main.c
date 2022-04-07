b5ec28872fdeb420be0e9e89a4dba7044f2762d3
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-04-07 13:20:54 -0500

    Create main.c

---
+++ sinlibreria/main.c
@@ -0,0 +1,68 @@
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+
+uint8_t p1 = 1;
+uint8_t p2 = 2;
+uint8_t p3 = 3;
+uint8_t p4 = 4;
+
+uint16_t demora = 500;
+
+int main()
+{ 
+    DDRB = (1 << p1) | (1 << p2) | (1 << p3) | (1 << p4); //pines 1,2,3,4 del puerto B como salida
+    PORTB |=  (1 << s1);
+    // Write your code here
+   while (1)
+      {
+
+	    PORTB |= (1 << p1);
+	    PORTB &= ~(1 << p2);
+	    PORTB &= ~(1 << p3);
+	    PORTB &= ~(1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB |= (1 << p1);
+	    PORTB |= (1 << p2);
+	    PORTB &= ~(1 << p3);
+	    PORTB &= ~(1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB &= ~(1 << p1);
+	    PORTB |= (1 << p2);
+	    PORTB &= ~(1 << p3);
+	    PORTB &= ~(1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB &= ~(1 << p1);
+	    PORTB |= (1 << p2);
+	    PORTB |= (1 << p3);
+	    PORTB &= ~(1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB &= ~(1 << p1);
+	    PORTB &= ~(1 << p2);
+	    PORTB |= (1 << p3);
+	    PORTB &= ~(1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB &= ~(1 << p1);
+	    PORTB &= ~(1 << p2);
+	    PORTB |= (1 << p3);
+	    PORTB |= (1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB &= ~(1 << p1);
+	    PORTB &= ~(1 << p2);
+	    PORTB &= ~(1 << p3);
+	    PORTB |= (1 << p4);
+	    _delay_ms (demora);
+	    
+     	    PORTB |= (1 << p1);
+	    PORTB &= ~(1 << p2);
+	    PORTB &= ~(1 << p3);
+	    PORTB |= (1 << p4);
+	    _delay_ms (demora);
+      }     
+}
