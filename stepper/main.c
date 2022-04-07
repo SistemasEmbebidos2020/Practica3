dc9bf04c9f4cfffbb9750d7d9a503829b2ece5ba
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-04-07 13:23:09 -0500

    Update main.c

--- stepper/main.c
+++ stepper/main.c
@@ -3,7 +3,9 @@
 int main()
  { 
   PORTB |= 1<<0;  //activar resistencia pullUp del pin 0 del puerto B
-  pasoapasoinit('d',1,2,3,4,100);   //iniciarlizar el control del motor paso a paso
+  //iniciarlizar el control del motor paso a paso
+  //primer se indica que puerto se desea usar b,c o d, y que pines serán usados para el control, por ultimo la velocidad.
+  pasoapasoinit('d',1,2,3,4,100);   
   while (1){
      if (!(PINB&(1<<0))){  //se pregunta por el pinB0 donde está conectado el interruptor
 	giro_izq();
