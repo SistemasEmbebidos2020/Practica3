a7adae6b3def90e7f45e335a99b39c73ae443071
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-04-29 10:36:14 -0500

    Update main.c

--- optimizado/main.c
+++ optimizado/main.c
@@ -1,14 +1,14 @@
 #include "func.h"
 
-uint16_t demora = 50;
+int demorar = 5;
 
 int main()
 { 
-   
+   stepinit('b',1,4);
    PORTB |= 1<<0;
    while(1){
-   if (!(PINB&(1<<0)))  horario(demora);
-   else antihorario(demora);
+   if (!(PINB&(1<<0)))  horario(demorar);
+   else antihorario(demorar);
    }
    return 0;
 }
